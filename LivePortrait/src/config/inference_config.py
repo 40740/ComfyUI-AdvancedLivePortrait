a65bb4fafcd130811bdf3298049abfd4f928f503
老耗 <39937268+40740@users.noreply.github.com>
2024-08-03 06:56:53 +0800

    Update inference_config.py

--- LivePortrait/src/config/inference_config.py
+++ LivePortrait/src/config/inference_config.py
@@ -8,11 +8,11 @@ current_directory = os.path.dirname(current_file_path)
 class InferenceConfig:
     def __init__(self):
         self.models_config: str = os.path.join(current_directory, "models.yaml")
-        self.checkpoint_F: str = os.path.join(folder_paths.models_dir, "liveportrait", "base_models", "appearance_feature_extractor.pth")
-        self.checkpoint_M: str = os.path.join(folder_paths.models_dir, "liveportrait", "base_models", "motion_extractor.pth")
-        self.checkpoint_G: str = os.path.join(folder_paths.models_dir, "liveportrait", "base_models", "spade_generator.pth")
-        self.checkpoint_W: str = os.path.join(folder_paths.models_dir, "liveportrait", "base_models", "warping_module.pth")
-        self.checkpoint_S: str = os.path.join(folder_paths.models_dir, "liveportrait", "retargeting_models", "stitching_retargeting_module.pth")
+        self.checkpoint_F: str = os.path.join(folder_paths.models_dir, "liveportrait", "appearance_feature_extractor.pth")
+        self.checkpoint_M: str = os.path.join(folder_paths.models_dir, "liveportrait", "motion_extractor.pth")
+        self.checkpoint_G: str = os.path.join(folder_paths.models_dir, "liveportrait", "spade_generator.pth")
+        self.checkpoint_W: str = os.path.join(folder_paths.models_dir, "liveportrait", "warping_module.pth")
+        self.checkpoint_S: str = os.path.join(folder_paths.models_dir, "liveportrait", "stitching_retargeting_module.pth")
         self.flag_use_half_precision: bool = True  # whether to use half precision
 
         self.input_shape: Tuple[int, int] = (256, 256)  # input shape
